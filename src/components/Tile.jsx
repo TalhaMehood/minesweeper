078ecc1692f641bbf3ec00bb5356c8d8add15dd1
TalhaMehood <talhamehmood1993@gmail.com>
2024-09-25 19:26:00 +0500

    Show and style content of the tile

--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -2,7 +2,21 @@ import classnames from 'classnames';
 import PropTypes from 'prop-types';
 
 const getTileContent = (tile) => {
+  if (!tile.isRevealed) {
+    return '';
+  }
 
+  if (tile.isMine) {
+    return '💣';
+  }
+
+  if (tile.number > 0) {
+    return tile.number;
+  }
+
+  if (tile.number === 0) {
+    return '';
+  }
 
   return '';
 };
@@ -11,6 +25,9 @@ const Tile = ({ tile, onClick }) => {
   const tileClass = classnames(
     'w-10 h-10 text-black flex items-center justify-center cursor-pointer rounded-sm',
     {
+      'bg-gray-400': tile.isRevealed && tile.number === 0,
+      'bg-gray-300': tile.isRevealed && tile.number > 0,
+      'bg-red-500': tile.isMine && tile.isRevealed,
       'bg-gray-700 hover:bg-gray-500': !tile.isRevealed,
     }
   );

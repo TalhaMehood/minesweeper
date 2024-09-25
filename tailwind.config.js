8bb3a46a497549f84268c9eb170e3252e4772948
TalhaMehood <talhamehmood1993@gmail.com>
2024-09-25 18:48:58 +0500

    Add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'], // Ensure you're scanning the right paths
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
